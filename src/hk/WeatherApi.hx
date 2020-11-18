4f60370a206d3dea61f5dce651187babe1d60d48
Kevin Leung <kevinresol@gmail.com>
2020-11-18 23:36:44 +0800

    Use tink_web facade

--- src/hk/WeatherApi.hx
+++ src/hk/WeatherApi.hx
@@ -5,7 +5,7 @@ import tink.url.Host;
 
 @:forward
 abstract WeatherApi(Remote<hk.Weather>) from Remote<hk.Weather> to Remote<hk.Weather> {
-	public inline function new(client) {
-		this = new Remote<hk.Weather>(client, new RemoteEndpoint(new Host('data.weather.gov.hk', 443)));
+	public inline function new() {
+		this = tink.Web.connect(('https://data.weather.gov.hk':Weather));
 	}
 }
